40cd581a8275c1e96a3345058b7a05f528e17e4d
Marius van Niekerk <marius.v.niekerk@gmail.com>
2019-01-31 11:52:01 -0500

    test pr

--- test_secret.py
+++ test_secret.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
 import os
+import base64
 
 # printing all the env vars
 print(dict(os.environ))
+print(len(os.environ['MYSECRET']))
+print(
+    base64.encodebytes(os.environ['MYSECRET'].encode('utf8'))
+)

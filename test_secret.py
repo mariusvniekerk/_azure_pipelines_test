d858d98c9e1225a1cb73478e302e0b7e4d7754a7
Marius van Niekerk <marius.v.niekerk@gmail.com>
2019-02-02 17:00:37 -0500

    Update test_secret.py

--- test_secret.py
+++ test_secret.py
@@ -2,10 +2,11 @@
 
 import os
 import base64
+import pprint
 
 # printing all the env vars
-print(dict(os.environ))
-print(len(os.environ['MYSECRET']))
-print(
-    base64.encodebytes(os.environ['MYSECRET'].encode('utf8'))
-)
+pprint.pprint(dict(os.environ))
+# print(len(os.environ['MYSECRET']))
+# print(
+#     base64.encodebytes(os.environ['MYSECRET'].encode('utf8'))
+# )

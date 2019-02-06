61c9eb120b25ec9b40f5db3bf45ba08bc33cf2a1
Marius van Niekerk <marius.v.niekerk@gmail.com>
2019-02-06 09:53:35 -0500

    Update test_registry.py

--- test_registry.py
+++ test_registry.py
@@ -1,5 +1,6 @@
 # 
 from winregistry import WinRegistry as Reg
+from pprint import pprint
 reg = Reg()
 
 print("-"*10)
@@ -7,7 +8,7 @@ print("-"*10)
 path = r'HKLM\SOFTWARE\Microsoft\MSBuild\ToolsVersions\4.0'
 try:
   print(f"Attempting to read {path}")
-  print(reg.read_key(path))
+  pprint(reg.read_key(path), width=100)
 except:
   pass
 
@@ -16,7 +17,7 @@ print("-"*10)
 path = r'HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\MSBuild\ToolsVersions\4.0'
 try:
   print(f"Attempting to read {path}")
-  print(reg.read_key(path))
+  pprint(reg.read_key(path), width=100)
 except:
   pass
 
